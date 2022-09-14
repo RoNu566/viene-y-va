2a70e23af7e319bae723eef451fb3e7445d31670
RoNu566 <rocion5666@gmail.com>
2022-09-13 22:14:41 -0300

    Desafio DOM

--- main.js
+++ main.js
@@ -1,64 +1,67 @@
 class Prenda {
-    constructor(tipo, talle, color, precio, disponibilidad) {
-        this.tipo = tipo
-        this.talle = talle
-        this.color = color
-        this.precio = precio
-        this.disponibilidad = disponibilidad
+    constructor(id,name, size, colour, price, image) {
+        this.id = id
+        this.name = name
+        this.size = size
+        this.colour = colour
+        this.price = price
+        this.image = image
     }
-    FueraDeStock() {
-        let error = "prenda no disponible"
-        if (this.disponibilidad == true) {
-            this.disponibilidad = false
 
-        } else {
-            console.log(error)
-        }
     }
-}
 
-const tienda = []
+const Clothes = []
 
-let Prenda1 = new Prenda("remera", "XL", "rojo", 500, false)
-let Prenda2 = new Prenda("remera", "M", "verde", 450, true)
-let Prenda3 = new Prenda("camisa", "S", "azul", 550, false)
-let Prenda4 = new Prenda("blusa", "XS", "rosado", 600, true)
-let Prenda5 = new Prenda("shorts", "M", "violeta", 1200, true)
+let Prenda1 = new Prenda(1,"remera", "XL", "rojo", 500, "red-t-shirt.jpg")
+let Prenda2 = new Prenda(2,"remera", "M", "verde", 450, "green-t-shirt.jpg")
+let Prenda3 = new Prenda(3,"camisa", "S", "azul", 550, "blue-shirt.jpg")
+let Prenda4 = new Prenda(4,"blusa", "XS", "rosado", 600, "pinkblouse.jpg")
+let Prenda5 = new Prenda(5,"shorts", "M", "violeta", 1200, "purple-shorts.jpg")
 
-tienda.push(Prenda1)
-tienda.push(Prenda2)
-tienda.push(Prenda3)
-tienda.push(Prenda4)
-tienda.push(Prenda5)
+Clothes.push(Prenda1,Prenda2,Prenda3,Prenda4,Prenda5)
 
-console.table(tienda)
-
-let disponible = tienda.filter(Prenda => Prenda.disponibilidad == true)
-console.log(disponible)
+console.table(Clothes)
 
 
-let ingresar = (confirm("Busca tu talle"))
-if (ingresar == true) {
-    let ingresar = (prompt("¿Que talle buscas?"))
-    let filtro = tienda.filter(Prenda => Prenda.talle == ingresar)
-    console.table(filtro)
+// let ingresar = (confirm("Busca tu talle"))
+//  if (ingresar == true) {
+//      let ingresar = (prompt("¿Que talle buscas?"))
+//     let filtro = Clothes.filter(clothes => Clothes.size == ingresar)
+//     console.table(filtro)
 
-}
+//  }
 
 function agregarprenda() {
-    let nombrenuevaprenda = (prompt("ingrese una prenda nueva"))
-    let tallenuevo = (prompt("ingrese el talle en letras, ej: S, M, L"))
-    let colornuevo = (prompt("ingrese el color de la prenda"))
-    let precionuevo = Number(prompt("ingrese el precio"))
-    let disponibilidadnueva = confirm("Acepta para confirmar si esta disponible")
-    let newprenda = new Prenda( nombrenuevaprenda, tallenuevo, colornuevo, precionuevo, disponibilidadnueva)
+    let idnuevo = (Clothes.length +1);
+    let nombrenuevaprenda = (prompt("ingrese una prenda nueva"));
+    let tallenuevo = (prompt("ingrese el talle en letras, ej: S, M, L"));
+    let colornuevo = (prompt("ingrese el color de la prenda"));
+    let precionuevo = Number(prompt("ingrese el precio"));
+    let image = "non-available.jpg"
+    let newprenda = new Prenda( idnuevo,nombrenuevaprenda, tallenuevo, colornuevo, precionuevo, image)
     console.log(newprenda)
-    tienda.push(newprenda)
-    console.log(tienda)
+    Clothes.push(newprenda)
+    console.log(Clothes)
+
+    loadClothes(Clothes)
 }
 
-let agregarnuevaprenda = (prompt("¿Desea ingresar una prenda nueva?")).toLocaleUpperCase()
-while (agregarnuevaprenda == "SI") {
-    agregarprenda()
-    agregarnuevaprenda = (prompt("¿Tenes otra prenda para cargar?"))
+
+const loadClothes = (Clothes) => {
+    let container = document.querySelector('#container');
+    for (const clothes of Clothes) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'card');
+        div.innerHTML = `
+            <img src="${clothes.image}" alt="${clothes.size}">
+            <h3>${clothes.price}</h3>
+            <h4>${clothes.colour}</h4>
+            <h4>${clothes.size}</h4>
+            <h4>${clothes.name}</h4>
+            <button class="button" id="${clothes.id}">Agregar al carrito</button>
+        `;
+        container.appendChild(div);
+    }
+
 }
+loadClothes(Clothes);
